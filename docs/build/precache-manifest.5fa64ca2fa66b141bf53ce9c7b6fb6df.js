00391f0b6ae33006af39878f9ed862b713c852ff
OFFICE\joseph.passineau <joseph.passineau@gsoft.com>
2019-01-24 22:02:15 -0500

    Added a new script to build docs

---
+++ docs/build/precache-manifest.5fa64ca2fa66b141bf53ce9c7b6fb6df.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "a78226ea9661fd4b87f52d8e6c802640",
+    "url": "/static/media/6_armies.a78226ea.png"
+  },
+  {
+    "revision": "3054d5b45387c08590f3",
+    "url": "/static/css/main.373ffa1c.chunk.css"
+  },
+  {
+    "revision": "0676798f49b878a3759ba6d8402ff268",
+    "url": "/static/media/wildlings_track.0676798f.png"
+  },
+  {
+    "revision": "9b3bb26eaddf25e7521a",
+    "url": "/static/js/1.9b3bb26e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce4d393cb5932094d87b4de4016c2d03",
+    "url": "/static/media/influence.ce4d393c.png"
+  },
+  {
+    "revision": "027b7bf20163129877e37dacdc465435",
+    "url": "/static/media/influence_targaryen.027b7bf2.png"
+  },
+  {
+    "revision": "0c407a57b178736ea6d156a48cf76c22",
+    "url": "/static/media/got.0c407a57.png"
+  },
+  {
+    "revision": "d035554c1b8488656654422c153a172f",
+    "url": "/static/media/castles.d035554c.png"
+  },
+  {
+    "revision": "716cbdc0a5b93d1b252934d4f96e88b7",
+    "url": "/static/media/castles_win.716cbdc0.png"
+  },
+  {
+    "revision": "940d492c572523e47251cbbaed46c4c8",
+    "url": "/static/media/game_round_marker.940d492c.png"
+  },
+  {
+    "revision": "bb5ff5b561c0eaea0f94380cc29626f0",
+    "url": "/static/media/influence_1stars.bb5ff5b5.png"
+  },
+  {
+    "revision": "8217db153f0e5d58ca0c4fc795082b6d",
+    "url": "/static/media/influence_2stars.8217db15.png"
+  },
+  {
+    "revision": "31340048c0ea4b22f5f522a046c031d9",
+    "url": "/static/media/influence_3stars.31340048.png"
+  },
+  {
+    "revision": "076490ab219fec703e0c2fb3399c45b1",
+    "url": "/static/media/influence_targaryen_3stars.076490ab.png"
+  },
+  {
+    "revision": "951b37e7b8b7b71ee709c3e120494f56",
+    "url": "/static/media/round.951b37e7.png"
+  },
+  {
+    "revision": "7792499131ab1057cb26be45f05abadb",
+    "url": "/static/media/supply_icon.77924991.png"
+  },
+  {
+    "revision": "1dc6a71887b057d16f56235e2b42ddb3",
+    "url": "/static/media/wildling.1dc6a718.png"
+  },
+  {
+    "revision": "caba7a563b2f9002558f7cb817ed3bce",
+    "url": "/static/media/Romic.caba7a56.ttf"
+  },
+  {
+    "revision": "5ab44da4915c7bd66fa9ba9998b7a5a1",
+    "url": "/static/media/0_armies.5ab44da4.png"
+  },
+  {
+    "revision": "88fdea23b6c073c2588c5da275f17cc5",
+    "url": "/static/media/1_armies.88fdea23.png"
+  },
+  {
+    "revision": "c56481fe72854181ccd44a8093c17e2d",
+    "url": "/static/media/2_armies.c56481fe.png"
+  },
+  {
+    "revision": "1702c1b78d567b6c0895a2ef5960e242",
+    "url": "/static/media/3_armies.1702c1b7.png"
+  },
+  {
+    "revision": "5aea8e6dab16cd0df92f3689353d26e4",
+    "url": "/static/media/4_armies.5aea8e6d.png"
+  },
+  {
+    "revision": "7130ff05170dd33e1538053d14773097",
+    "url": "/static/media/5_armies.7130ff05.png"
+  },
+  {
+    "revision": "3054d5b45387c08590f3",
+    "url": "/static/js/main.3054d5b4.chunk.js"
+  },
+  {
+    "revision": "d3b39a5c7e6b83d15f88edb2b9f02f48",
+    "url": "/static/media/arryn_token.d3b39a5c.png"
+  },
+  {
+    "revision": "b0a42527d598ca07cf84b4a6879422e9",
+    "url": "/static/media/baratheon_token.b0a42527.png"
+  },
+  {
+    "revision": "2c8eb4857a9b14b958f85815637e901d",
+    "url": "/static/media/greyjoy_token.2c8eb485.png"
+  },
+  {
+    "revision": "46b3e562780b305163c7c89b08a69f27",
+    "url": "/static/media/house_arryn.46b3e562.png"
+  },
+  {
+    "revision": "9b80d4f81b4f71c8d903d0a34613100f",
+    "url": "/static/media/house_baratheon.9b80d4f8.png"
+  },
+  {
+    "revision": "b33e54b5d57931b26ddcdd53918f8c8f",
+    "url": "/static/media/house_greyjoy.b33e54b5.png"
+  },
+  {
+    "revision": "63353d14902e576edd0e37c707ff2563",
+    "url": "/static/media/house_lannister.63353d14.png"
+  },
+  {
+    "revision": "0473968e14608c35152aaad7f56dc76b",
+    "url": "/static/media/house_martell.0473968e.png"
+  },
+  {
+    "revision": "bb0369a7537b370648d510b34cc766b8",
+    "url": "/static/media/house_stark.bb0369a7.png"
+  },
+  {
+    "revision": "27a7dbafe8b54b1d7a4fdc2ee479099e",
+    "url": "/static/media/house_targaryen.27a7dbaf.png"
+  },
+  {
+    "revision": "df512deb641436cad2688d5bbf76301c",
+    "url": "/static/media/house_tyrell.df512deb.png"
+  },
+  {
+    "revision": "9fad924bbec9085a28a5db3e7088bf1f",
+    "url": "/static/media/influence_track.9fad924b.png"
+  },
+  {
+    "revision": "f843a3e94c62223df2cccfb13d41eff3",
+    "url": "/static/media/influence_track_expansion.f843a3e9.png"
+  },
+  {
+    "revision": "3aa3b7ad0f1d5221a8eb02364d68c9a6",
+    "url": "/static/media/iron_throne_token.3aa3b7ad.png"
+  },
+  {
+    "revision": "2b6f1825acbfdff77d9b68a1c88b16fa",
+    "url": "/static/media/lannister_token.2b6f1825.png"
+  },
+  {
+    "revision": "44d14c2afac277309467694f4bc057ef",
+    "url": "/static/media/martell_token.44d14c2a.png"
+  },
+  {
+    "revision": "46a8db2ffca2e0fcac06846beff62cf1",
+    "url": "/static/media/messenger-raven-token.46a8db2f.png"
+  },
+  {
+    "revision": "d17575225248b847015955c4aee4316d",
+    "url": "/static/media/round&victory_tracks.d1757522.png"
+  },
+  {
+    "revision": "f36a0d6a625230f45b5b548e46ac28f7",
+    "url": "/static/media/stark_token.f36a0d6a.png"
+  },
+  {
+    "revision": "f459b7f19b8bf12cf2a1b3a72dcc1416",
+    "url": "/static/media/supply_track.f459b7f1.png"
+  },
+  {
+    "revision": "69d89981220192b88984c8c02e3c021e",
+    "url": "/static/media/targaryen_token.69d89981.png"
+  },
+  {
+    "revision": "488cb5f0b7b4ff3e37249efaedd41d53",
+    "url": "/static/media/tyrell_token.488cb5f0.png"
+  },
+  {
+    "revision": "940de2623f4bdb89ffdb9a85f0b62811",
+    "url": "/static/media/valyrian-steel-blade-token.940de262.png"
+  },
+  {
+    "revision": "9b3bb26eaddf25e7521a",
+    "url": "/static/css/1.2b1a5419.chunk.css"
+  },
+  {
+    "revision": "6ffae1d1f61332ada0f781aec3d10fef",
+    "url": "/index.html"
+  }
+];
